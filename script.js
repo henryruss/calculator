98a763cbaccc94913054f7ab01236d667a609a2e
Henry Russell <henry.russell28@gmail.com>
2024-11-08 16:30:17 +1000

    Made the calculator work!! Actually pretty happy about this. Have not gone through and fixed all pesky errors but I do not want to do that so I think I am going to move on.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function multiply (a,b){
 }
 function divide (a,b){
     if (b === 0) {
-        throw new Error("Cannot divide by zero");
+        throw new Error("Cannot divide by zero!");
       }
       return a / b;
 }
@@ -31,6 +31,72 @@ function operate(operator,number1,number2){
 }
 
 
-let num1 = 0;
-let num2 = 0;
-let operation = "+";
+let num1 = '';
+let num2 = '';
+let operation ='';
+
+const currentDisplay = document.getElementById("display");
+function clearDisplay(){
+    currentDisplay.textContent = display;
+    display = '';
+    num1='';
+    num2='';
+    operation='';
+}
+
+let display = '';
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+numbers.forEach(button => {
+    button.addEventListener('click', () => {
+        display += button.textContent;
+        currentDisplay.textContent = display;
+        
+    })
+});
+const clear = document.getElementById("clear");
+clear.addEventListener("click", function(){
+    display = 0;
+    clearDisplay();
+});
+
+function getNumber(button){
+    const num = button.textContent;
+}
+
+let num1Length = 0;
+operators.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        num1 = display;
+        operation = button.textContent;
+        display += button.textContent;
+        currentDisplay.textContent = display;
+        num1Length = display.length;
+    })
+})
+
+let equal = document.getElementById("equals");
+equal.addEventListener('click', ()=>{
+    num2 = display.substring(num1Length);
+    let number1 = Number(num1);
+    let number2 = Number(num2);
+    if (operation == "+"){
+        display = add(number1,number2);
+        currentDisplay.textContent = display;
+    }
+    if (operation == "-"){
+        display = subtract(number1,number2);
+        currentDisplay.textContent = display;
+    }
+    if (operation == "*"){
+        display = multiply(number1,number2);
+        currentDisplay.textContent = display;
+    }
+    if (operation == "/"){
+        display = divide(number1,number2);
+        currentDisplay.textContent = display;
+    }
+});
+
+
+
